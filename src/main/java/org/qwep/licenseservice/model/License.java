a2e6bda0721d9df045435dd496b3c70446b4a8a6
codevarius <koshaevk@gmail.com>
2021-09-09 00:52:50 +0300

    Initial commit

---
+++ src/main/java/org/qwep/licenseservice/model/License.java
@@ -0,0 +1,59 @@
+package org.qwep.licenseservice.model;
+
+public class License {
+
+    private Integer id;
+    private String licenseId;
+    private String organizationId;
+    private String description;
+    private String productName;
+    private String licenseType;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLicenseId() {
+        return licenseId;
+    }
+
+    public void setLicenseId(String licenseId) {
+        this.licenseId = licenseId;
+    }
+
+    public String getOrganizationId() {
+        return organizationId;
+    }
+
+    public void setOrganizationId(String organizationId) {
+        this.organizationId = organizationId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getLicenseType() {
+        return licenseType;
+    }
+
+    public void setLicenseType(String licenseType) {
+        this.licenseType = licenseType;
+    }
+}
